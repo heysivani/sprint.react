53617cda308bb2ad9a9b5f0d5ac3bb8f27eb6530
heysivani <heysivani@gmail.com>
2020-10-21 11:39:16 +0900

    initial commit; App.jsx WIP
    Co-authored-by: OnigiriJack <onigirijack@users.noreply.github.com>
    Co-authored-by: heysivani <heysivani@users.noreply.github.com>

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,8 +2,13 @@ import React from "react";
 import "../styles/styles.css";
 
 export default function App() {
+  const [currentView, setCurrentView] = useState("AllPhotos");
+  const [photos, setPhotos] = useState([]);
+  const [selectedPhoto, setSelectedPhoto] = useState("");
+
   return (
     <div className="app">
+      <div className="navbar">Hi</div>
       <h1>Hello World!</h1>
     </div>
   );
