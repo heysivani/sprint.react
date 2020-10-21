0033d2cef01dec97bfbf5c1667f176ca8bf71069
heysivani <heysivani@gmail.com>
2020-10-21 12:32:09 +0900

    Retrieves list of images and maps their keys correctly
    Co-authored-by: OnigiriJack <onigirijack@users.noreply.github.com>
    Co-authored-by: heysivani <heysivani@users.noreply.github.com>

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,11 +3,22 @@ import "../styles/styles.css";
 import Navbar from "./Navbar";
 import SinglePhoto from "./SinglePhoto";
 import AllPhotos from "./AllPhotos";
+import { listObjects } from "../utils/index.js";
 
 export default function App() {
   const [currentView, setCurrentView] = useState("AllPhotos");
   const [photos, setPhotos] = useState([]);
   const [selectedPhoto, setSelectedPhoto] = useState("");
+
+  listObjects() // add all of the Keys to photos?
+    .then(pics => {
+      const picKeys = pics.map(pic => pic.Key);
+      return picKeys;
+    })
+    .then(picKeys => {
+      setPhotos(picKeys);
+    });
+  console.log(photos);
 
   return (
     <div className="app">
